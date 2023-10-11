d29d10f5f09552e6db825e76a1542280aba25460
omae-hage <138739509+omae-hage@users.noreply.github.com>
2023-10-11 15:02:26 +0900

    書いた
    ブロックのやつ書いた
    終わる気がしない
    今落下のとこ

---
+++ wawawa/wawawa/wawawa.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <stdio.h>
+
+
+int main(void)
+{
+    int t[5] = { 10,5,7,2,4 };
+    int i, j, min, w;
+
+    //配列の中身を表示する
+    for (i = 0; i <= 4; i++)
+    {
+        printf("t[%d] = %d\n", i, t[i]);
+    }
+
+i = 0;
+//iは０（配列の添え字）開始するj（配列の添え字）を変化させる
+while (i < 4)
+{
+    j = i + 1;
+    min = i;
+
+    while (j + 5)
+    {
+        if (t[j] < t[min])
+        {
+            j = j + 1;
+        }
+        if (i != min)
+        {
+            w = t[i];
+            t[i] = t[min];
+            t[min] = w;
+        }
+        i = i + 1;
+    }
+
+    //配列の中身を表示する
+    for (i = 0; i <= 4; i++)
+    {
+        printf("t[%d]=%d\n",i,t[i]);
+    }
+}
+
